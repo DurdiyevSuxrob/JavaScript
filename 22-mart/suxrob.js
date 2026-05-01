38b8b37cb1fef2686dc7787df275a58cc104846e
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-05-01 17:31:33 +0500

    start

---
+++ 22-mart/suxrob.js
@@ -0,0 +1,22 @@
+//Sonlarni qo'shish va ayirish 
+let birinchison = 3
+let ikkinchison = 4
+let uchinchison = 899
+
+
+
+let ayirish = birinchison - ikkinchison
+let qoshish = uchinchison + birinchison
+let hamma_Yigindi = birinchison+ikkinchison+uchinchison
+
+console.log("Ayirish", ayirish)
+console.log("Qoshish natijasi", qoshish)
+console.log("Hammasi qoshildi birbirsina ishqilib", hamma_Yigindi)
+
+
+let ism = "suxrob" 
+let familiya = "Durdiyev"
+
+let isimlarniqoshish = ism + familiya
+
+console.log("ism", isimlarniqoshish)
