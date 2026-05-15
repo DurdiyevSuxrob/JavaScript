0a5285350f05e4162f0ed93a8e81de93a03582d6
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-05-15 18:10:48 +0500

    add

---
+++ 22-mart/azamat.js
@@ -0,0 +1,48 @@
+// Voyaga yetgan va yetmaganlikini aniqlash
+// let yosh = 12
+// if (yosh > 17) {
+//     console.log("Siz voyaga yetgansiz")
+// } else {
+//     console.log("Siz voyaga yetmagansiz")
+// }
+// =====================================
+// let son = 13
+// if (son % 2 === 0) {
+//     console.log("Siz juft son kiritdingiz")
+// } else {
+//     console.log("Siz toq son kiritdingiz")
+// }
+// 3. o'quvchi sinifini kiritsin, agarda 11sonidan katta 
+// let sinf = 12
+// if (sinf < 12)  {
+//     console.log("Siz hali oquvchisiz")
+// } else {
+//     console.log("Siz maktab tugatgansiz")
+// }
+//4
+// let havo = 15
+// if (havo > 30) {
+//     console.log("bugunki havo issiq")
+// } else {
+//     console.log("bugunki havo yaxshi")
+// }
+// let son = 0
+// if (son < 0 ) {
+//     console.log("bu son manfiy")
+// } else if(son === 0) {
+//     console.log("bu son nol")
+// } 
+
+// else {
+//     console.log("bu son musfat")
+// }
+// ===============================
+let parol = "qweqwe"
+if (parol === "qweqwe") {
+    console.log("Tizim ochildi")
+} else {
+    console.log("Parol xato")
+}
+
+
+    
