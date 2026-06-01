b868cd4d5cecb5e0a75c53f2acd8d308e5c119c8
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-06-01 17:51:41 +0500

    add

--- 1-iyun/daes1.js
+++ 1-iyun/daes1.js
@@ -5,12 +5,16 @@
 //  for (let boshlanishi =3; boshlanishi <= 23; boshlanishi++) {
 //     console.log(boshlanishi);
 //  }
-for (let i=0; i<101; i++) {
-    if (i%2 !==0) {
-        console.log(i);
-        
-    }
+// for (let i=0; i<101; i++) {
+//     if (i%2 !==0) {
+//         console.log(i);
+//     }
+// }
+for (let i = 0; i <= 100; i=i++ ) {
+    if ( i % 4 )
+    console.log(i);
 }
+
 
 
 
