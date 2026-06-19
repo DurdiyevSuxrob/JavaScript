28bf76d144986027d3e70a44b814a22a6cd5f1fd
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-06-19 17:26:52 +0500

    add

--- 1-iyun/amaliyot.js
+++ 1-iyun/amaliyot.js
@@ -54,7 +54,7 @@
 // }
 // else {
 //     console.log("marfiy");
-    
+
 // }
 // ============================================
 // for (let i = 2; i <= 8; i++) {
@@ -67,36 +67,99 @@
 // do {
 //     if (i % 2 == 0 ) {
 //         console.log(i ** 2);
-        
+
 //     }
 //     i++
 // } while(i < 100)
 // ===============qub chiqarish==========================================
-let i = 1
-do {
-    if (i % 1 == 0 ) {
-        console.log(i ** 3);
-        
-    }
-    i++
-} while(i < 100)
+// let i = 1
+// do {
+//     if (i % 1 == 0 ) {
+//         console.log(i ** 3);
+
+//     }
+//     i++
+// } while(i < 100)
+// =============1-masala===================================
+// let i = 5
+// do {
+//     console.log(i);
+//     i--
+
+// } while (i > 0)
+// ===========2-masala===================================
+// let i = 0
+// do {
+//     console.log("Hello")
+//     i++
+// } while(i < 3)
+// =====3-masala=======================================================
+// let i = 1
+// do {
+//     if (i % 2 == 0 ) {
+//         console.log(i  );
+
+//     }
+//     i++
+// } while(i < 11)
+// ========4-masala=======================================
+// let i = 1
+// do {
+//     if (i % 2 !== 0 ) {
+//         console.log(i  );
+
+//     }
+//     i++
+// } while(i < 10)
+// =======5-masala=======================================
+//  let i = 0
+// do {
+//     console.log("3")
+//     i++
+// } while(i < 5)
+// =================================================
+// =========Array================================
+let names = ["Saidjon", "Shohjahon", "Azamat", "Suxrob", "Jahongir"];
+// console.log(names);
+console.log(names[0]);
+console.log(names[2]);
+console.log(names[4]);
+console.log(names[3]);
+
+let massiv = new Array(4); //  uzunligi 4ga teng bosh arrey
+let massiv0 = new Array(4, 1, 2, 3, 5);
+let massiv1 = [
+  "Saidjon",
+  "Shohjahon",
+  "Azamat",
+  "Suxrob",
+  "Jahongir",
+  "Bexzod",
+  "Behruz",
+  "mominjon",
+  "kamron",
+  "Shahruzbek",
+  "Bobur",
+  "doston",
+  "Abbos"
+];
+ //console.log(names.length)
+ for(let i = 0; i < names.length; i++){
+    console.log(names[i]);
+// for (let ozgaruvchi of names){
+//     console.log(ozgaruvchi);
+    
+// }
+    
+ }
+
+// console.log(massiv);
+// console.log(massiv0);
+// console.log(massiv1);
+// console.log(names.length);
+//^ Arrey methods
+let pushMethod = names.push("oxirigaqoshildi") //oxiriga qoshiladi
+console.log(names);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let 
