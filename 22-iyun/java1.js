9bb044e2c105e9a45048741ff0aa0e365fcf0ee5
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-06-22 17:25:54 +0500

    add

---
+++ 22-iyun/java1.js
@@ -0,0 +1,63 @@
+// for (let i = 0; i<= 10; i++) {
+//     console.log(i);
+    
+// }
+// ================================================
+// let i = 1;
+// while (i <= 20) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+        
+//     }
+//     i++
+// }
+// ===============================================
+// let son = 0
+// do {
+//     son = parseInt(prompt("Soni kiriting"))
+// } while (son !== 0)
+//     console.log(0);
+// ==================================================
+// let arr = ["Shohruz", 12, "46", false, true, undefined, Boolean, Number, String]
+// for (let i of arr) {
+//     console.log(i);
+    
+// }
+// ===============================================================================
+// let arr = [-1, 1, 3, -12, 6, 4, -22, 6]
+// let musbat = []
+// for (let i of arr) {
+//     if (i > 0) {
+//         musbat.push(i)
+//     }
+        
+// }
+// console.log(musbat.length);
+// ==================================================
+let 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
