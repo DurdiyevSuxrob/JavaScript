0003b8195253a806cdffa818a5630d6710bd3651
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-07-02 13:26:04 +0500

    add

--- 22-iyun/java2.js
+++ 22-iyun/java2.js
@@ -1,13 +1,16 @@
-// 1-masala
+// 1-masala%
 // let arr = [1, [2, [3, [4, 5]]], 6]
 // console.log([1, [2, [3, [4, 5]]], 6].flat(Infinity))
 // =============================================
-// 2-masala
+// 2-masala%
 // let arr = ["olma", "anor"];
 // arr.push("uzum");
 // console.log(arr); 
 // =================================================
-// 3-masala
-let talabalar = [{ism:"Ali", ball:75}, {ism:"Vali", ball:40}, {ism:"Guli", ball:90}];
-let idx = talabalar.findIndex((n) => n > );
-console.log(idx); // 2
+// 3-masala!
+// let talabalar = [{ism:"Ali", ball:75}, {ism:"Vali", ball:40}, {ism:"Guli", ball:90}];
+// let idx = talabalar.findIndex((n) => n > 35);
+// console.log(idx); // 2
+// ================================================================
+// 4-masala
+
