288eccd4f3ec7c5b5a4bcf1ab4bacffcb9011a8b
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-07-02 13:47:54 +0500

    add

--- 22-iyun/java2.js
+++ 22-iyun/java2.js
@@ -1,16 +1,35 @@
-// 1-masala%
+// 1-masala% massivini to'liq (Infinity chuqurlikda) tekislang.
+
 // let arr = [1, [2, [3, [4, 5]]], 6]
 // console.log([1, [2, [3, [4, 5]]], 6].flat(Infinity))
 // =============================================
-// 2-masala%
+// 2-masala% Bo'sh "savat" massivi yarating. Foydalanuvchi tanlagan 3 ta mahsulot nomini (o'zingiz kiriting)
+// push() yordamida qo'shing va oxirida savatdagi mahsulotlar sonini chop eting.
+
 // let arr = ["olma", "anor"];
 // arr.push("uzum");
 // console.log(arr); 
 // =================================================
-// 3-masala!
+// 3-masala! Balli 50 dan past bo'lgan birinchi talabaning indeksini findIndex() bilan toping.
+
 // let talabalar = [{ism:"Ali", ball:75}, {ism:"Vali", ball:40}, {ism:"Guli", ball:90}];
 // let idx = talabalar.findIndex((n) => n > 35);
 // console.log(idx); // 2
 // ================================================================
-// 4-masala
+// 4-masala% 2-indeksdan boshlab 2 ta elementni o'chirib, o'rniga 100 va 200 sonlarini joylashtiring. Natijani chop eting.
+
+// let arr = [10, 20, 30, 40, 50]
+// arr.splice(1, 2, "x")
+// console.log(arr);
+// ================================================
+// 5-masala% keys() yordamida faqat juft indekslarni (0, 2, ...) konsolga chiqaring.
+
+//  let mevalar = ["olma", "nok", "shaftoli", "gilos"];
+//  for (let index of mevalar.keys()) {
+//   if (index % 2 === 0) {
+//     console.log(index);
+//   }
+// }
+// =============================================================
+// 6-masala
 
