b5a381ca05e244dfdf0f76a8d610ff89ce5f7cfa
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-07-09 14:19:03 +0500

    add

---
+++ 22-iyun/java3.js
@@ -0,0 +1,5 @@
+// 1-masala%
+// let matn = "salom dunyo";
+// let natija = matn.toUpperCase();
+// console.log(natija);
+// =====================================================================================
