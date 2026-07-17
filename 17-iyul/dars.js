9803420d41a8b1f8be089a82b7cc707ba8cef6d5
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-07-17 17:24:56 +0500

    add

--- 17-iyul/dars.js
+++ 17-iyul/dars.js
@@ -1,7 +1,14 @@
-function ekranga matn chiqar() {
-    console.log( ekran); 
-    
+// function sonKop(a, b, c) {
+//     console.log(a * b * c);
+//      console.log(a / b / c);
+// }
+// sonKop(10, 15, 20)
+// ======================================================================================
+const son = function (a, b, c) {
+    console.log(a * b * c);
+    console.log(a / b / c);
 }
+son(10, 20, 30)
  
 
 
