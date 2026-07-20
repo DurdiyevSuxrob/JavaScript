81c7fe145071bd116a9d31a9f83b153d4c61d921
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-07-20 17:28:26 +0500

    add

--- 22-iyun/obyekt.js
+++ 22-iyun/obyekt.js
@@ -1,56 +1,73 @@
-const shoxruzbek = {
-  ism: "Shoxruzbek",
-  familiya: "Karimov",
-  yosh: 8,
-  jins: "Erkak",
-  tugilganYil: 2018,
-  tugilganOy: "Mart",
-  tugilganKun: 15,
-  millati: "O'zbek",
-  fuqaroligi: "O'zbekiston",
-  shahri: "Toshkent",
-  tumani: "Yunusobod",
-  maktab: "12-maktab",
-  sinf: 2,
-  harfi: "A",
-  boyi: 130,
-  vazni: 28,
-  sochRangi: "Qora",
-  kozRangi: "Jigarrang",
-  sevimliFan: "Matematika",
-  sevimliRang: "Ko'k",
-  sevimliTaom: "Osh",
-  sevimliIchimlik: "Sharbat",
-  sevimliMeva: "Olma",
-  sevimliSport: "Futbol",
-  sevimliJamoa: "Real Madrid",
-  sevimliOyini: "Minecraft",
-  sevimliHayvon: "Sher",
-  sevimliKitob: "Zumrad va Qimmat",
-  sevimliMultfilm: "Tom va Jerry",
-  hobbi: "Rasm chizish",
-  orzusi: "Dasturchi bo'lish",
-  telefoniBor: false,
-  ukalariSoni: 1,
-  opalariSoni: 0,
-  dostlariSoni: 12,
-  yaxshiKoradiganFasl: "Yoz",
-  yaxshiKoradiganBayram: "Navro'z",
-  kompyuterBiladi: true,
-  inglizTiliniOrganmoqda: true,
-  baholari: [5, 5, 4, 5, 5]
+// const shoxruzbek = {
+//   ism: "Shoxruzbek",
+//   familiya: "Karimov",
+//   yosh: 8,
+//   jins: "Erkak",
+//   tugilganYil: 2018,
+//   tugilganOy: "Mart",
+//   tugilganKun: 15,
+//   millati: "O'zbek",
+//   fuqaroligi: "O'zbekiston",
+//   shahri: "Toshkent",
+//   tumani: "Yunusobod",
+//   maktab: "12-maktab",
+//   sinf: 2,
+//   harfi: "A",
+//   boyi: 130,
+//   vazni: 28,
+//   sochRangi: "Qora",
+//   kozRangi: "Jigarrang",
+//   sevimliFan: "Matematika",
+//   sevimliRang: "Ko'k",
+//   sevimliTaom: "Osh",
+//   sevimliIchimlik: "Sharbat",
+//   sevimliMeva: "Olma",
+//   sevimliSport: "Futbol",
+//   sevimliJamoa: "Real Madrid",
+//   sevimliOyini: "Minecraft",
+//   sevimliHayvon: "Sher",
+//   sevimliKitob: "Zumrad va Qimmat",
+//   sevimliMultfilm: "Tom va Jerry",
+//   hobbi: "Rasm chizish",
+//   orzusi: "Dasturchi bo'lish",
+//   telefoniBor: false,
+//   ukalariSoni: 1,
+//   opalariSoni: 0,
+//   dostlariSoni: 12,
+//   yaxshiKoradiganFasl: "Yoz",
+//   yaxshiKoradiganBayram: "Navro'z",
+//   kompyuterBiladi: true,
+//   inglizTiliniOrganmoqda: true,
+//   baholari: [5, 5, 4, 5, 5]
+// };
+// shoxruzbek.lager = "Lagerga bormadi"
+// console.log(Object.values(shoxruzbek));
+// console.log(Object.keys(shoxruzbek));
+// console.log(Object.entries(shoxruzbek)[0]);
+// console.log(Object.entries(shoxruzbek)[1]);
+// console.log(Object.entries(shoxruzbek)[2]);
+// console.log(Object.entries(shoxruzbek)[3]);
+// console.log(Object.entries(shoxruzbek)[4]);
+// console.log(Object.entries(shoxruzbek)[5]);
+// console.log(Object.entries(shoxruzbek)[6]);
+// =======================================================================================
+const Obyekt = {
+  ism: "suxrob",
+  kasbi: "Dsturchi",
+  arr: [1,2,3,4,5,6],
+  suhrob: function () {
+    for (let i of this.arr) {
+      if (i % 2 == 0) {
+        console.log(i);
+      }
+    }
+  },
+  arrowFunksiya: () => {
+    console.log(this.ism);
+  },
 };
-shoxruzbek.lager = "Lagerga bormadi"
-console.log(Object.values(shoxruzbek));
-console.log(Object.keys(shoxruzbek));
-console.log(Object.entries(shoxruzbek)[0]);
-console.log(Object.entries(shoxruzbek)[1]);
-console.log(Object.entries(shoxruzbek)[2]);
-console.log(Object.entries(shoxruzbek)[3]);
-console.log(Object.entries(shoxruzbek)[4]);
-console.log(Object.entries(shoxruzbek)[5]);
-console.log(Object.entries(shoxruzbek)[6]);
-
+Obyekt.suxrob();
+Obyekt.arrowFunksiya();
 
 
 
