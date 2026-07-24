a88b25805d3d6784ad9dead6c94a1c59365f909d
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-07-24 17:14:40 +0500

    add

--- 17-iyul/dars.js
+++ 17-iyul/dars.js
@@ -17,11 +17,11 @@
 // function userData(){
 //     let ism = prompt("Isimingizni kiriting")
 //     let familiya = prompt("Familiyangizni kiriting")
-   
+
 //     let malumoti = `Sizning ismingiz ${ism}, familiyangiz ${familiya} va yoshingiz ${yosh}da!`
 
 //     console.log(malumoti);
-    
+
 // }
 // userData()
 // =============================================================================
@@ -34,11 +34,11 @@
 // function darajaa(b){
 //     console.log(b**3);
 //     return "Return funksiya qaytaradi"
-    
+
 // }
 //  darajaa(2)
 //  console.log(darajaa(2));
- 
+
 // ====================================================================================
 // function qoshish(a, b) {
 //     return a + b;
@@ -54,7 +54,7 @@
 //     } else {
 //         return "b soni katta"
 //     }
-// } 
+// }
 // console.log(masala1(2, 3));
 // ===================================================================================================
 // function masala2(son) {
@@ -68,31 +68,15 @@
 // }
 // console.log(masala2(-1));
 // ================================================================================================
-    function masala3(yosh) {}
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function masala3(yosh) {
+  if (yosh < 0) {
+    return "Iltimos manfiy son kiritmang";
+  } else if (yosh >= 0 && yosh <= 12) {
+    return "bolalik";
+  } else if (yosh >= 13 && yosh <= 17) {
+    return "o'smirlik";
+  } else {
+    return "katta";
+  }
+}
+console.log(masala3(18));
