28f7d0802301ac07c4e8d76f63e9789a5eca07c2
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-03 17:09:12 +0500

    add

--- avgust/java10.js
+++ avgust/java10.js
@@ -1,6 +1,7 @@
-const hozir = new Date();
-console.log(hozir);
-const sana3 = new Date("2026-08-03");
-
+// const hozir = new Date();
+// console.log(hozir);
+// const sana3 = new Date("2026-08-03");
+// ======================================================================================
+const
 
 
