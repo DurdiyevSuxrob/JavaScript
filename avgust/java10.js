15ca0071bd925e3ecce3637f6d34c1ecc259294e
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-03 17:09:51 +0500

    add

--- avgust/java10.js
+++ avgust/java10.js
@@ -2,6 +2,8 @@
 // console.log(hozir);
 // const sana3 = new Date("2026-08-03");
 // ======================================================================================
-const
+const juma = new Date(2026, 7, 7); // 2026-yil 7-avgust - bu juma kuni
+console.log(juma.getDate()); // 7 (oyning kuni)
+console.log(juma.getDay()); // 5 (haftaning kuni - juma)
 
 
