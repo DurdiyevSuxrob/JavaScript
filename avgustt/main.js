56e87de149ddea004713e94f67e0a1d270440562
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-07 17:34:43 +0500

    add

--- avgustt/main.js
+++ avgustt/main.js
@@ -1,6 +1,11 @@
 const box = document.getElementById('box')
+const dest = document.getElementById('dest')
 console.log(box);
 box.style.backgroundColor = "blue"
 box.style.width = "500px"
 box.style.height = "500px"
 box.style.borderRadius = "50%"
+console.log(dest);
+dest.style.width = "2000px"
+dest.style.height = "500px"
+dest.style.border = "1px" "soled"
