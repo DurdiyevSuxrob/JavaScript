7f7e302f8584fe93c7ba1ccedcf3bfeb6f419d7f
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-09 10:14:35 +0500

    add

--- avgustt/main.js
+++ avgustt/main.js
@@ -1,11 +1,75 @@
 const box = document.getElementById('box')
 const dest = document.getElementById('dest')
+const main2 = document.getElementById('main2')
+const main3 = document.getElementById('main3')
+const main4 = document.getElementById('main4')
+const main5 = document.getElementById('main5')
+const main6 = document.getElementById('main6')
+const main7 = document.getElementById('main7')
 console.log(box);
 box.style.backgroundColor = "blue"
 box.style.width = "500px"
 box.style.height = "500px"
 box.style.borderRadius = "50%"
 console.log(dest);
-dest.style.width = "2000px"
-dest.style.height = "500px"
-dest.style.border = "1px" "soled"
+// =================================================
+dest.style.width = "1800px"
+dest.style.height = "800px"
+dest.style.border = "1px solid black" 
+dest.style.margin = "auto"
+dest.style.marginTop = "50px"
+dest.style.display = "flex";
+dest.style.justifyContent = "space-around";
+dest.style.alignItems = "center";
+dest.style.borderRadius = "10px"
+
+
+
+
+
+
+
+main2.style.width = "250px"
+main2.style.height = "250px"
+main2.style.border = "1px solid black"
+main2.style.backgroundColor = "red"
+dest.style.borderRadius = "10px"
+dest.style.borderRadius = "10px"
+
+// =====================================================
+main3.style.width = "250px"
+main3.style.height = "250px"
+main3.style.border = "1px solid black"
+main3.style.backgroundColor = "yellow"
+main3.style.borderRadius = "10px"
+
+// ======================================================
+main4.style.width = "250px"
+main4.style.height = "250px"
+main4.style.border = "1px solid black"
+main4.style.backgroundColor = "green"
+main4.style.borderRadius = "10px"
+
+// =======================================================
+main5.style.width = "250px"
+main5.style.height = "250px"
+main5.style.border = "1px solid black"
+main5.style.backgroundColor = "blue"
+main5.style.borderRadius = "10px"
+
+// ========================================================
+main6.style.width = "250px"
+main6.style.height = "250px"
+main6.style.border = "1px solid black"
+main6.style.backgroundColor = "gold"
+main6.style.borderRadius = "10px"
+
+// ======================================================
+main7.style.width = "250px"
+main7.style.height = "250px"
+main7.style.border = "1px solid black"
+main7.style.backgroundColor = "brown"
+main7.style.borderRadius = "10px"
+
+// ==========================================================
+
