f518fe65415903a7d36b9e4b89085ba34377b8c9
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-10 17:52:02 +0500

    add

---
+++ avgustt/main2.js
@@ -0,0 +1,14 @@
+const div = document.getElementById('div')
+const div1 = document.getElementById('div1')
+// ==========================================================
+div.style.width = "1900px"
+div.style.height = "600px"
+div.style.border = "1px solid black"
+div.style.margin = "auto"
+div.style.marginTop = "100px"
+// =============================================
+div1.style.width = "300px"
+div1.style.height = "400px"
+div1.style.border = "1px solid black"
+div1.style.fontSize = "30px"
+div1.style.overflow = ""
