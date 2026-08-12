68cff6bd92ff537bc19094d4e315ef5f0eaaea18
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-12 11:21:27 +0500

    add

--- avgustt/main2.js
+++ avgustt/main2.js
@@ -1,14 +1,50 @@
 const div = document.getElementById('div')
 const div1 = document.getElementById('div1')
+const div2 = document.getElementById('div2')
+const div3 = document.getElementById('div3')
+const div4 = document.getElementById('div4')
+
+
+
 // ==========================================================
 div.style.width = "1900px"
 div.style.height = "600px"
 div.style.border = "1px solid black"
 div.style.margin = "auto"
 div.style.marginTop = "100px"
+div.style.display = "flex"
+div.style.justifyContent = "space-around"
+div.style.alignContent = "center"
+div.style.alignItems = "center"
+div.style.borderRadius = "30px"
+div.style.boxShadow = "2px 2px 2px 0px 0px 5px"
+
 // =============================================
 div1.style.width = "300px"
 div1.style.height = "400px"
 div1.style.border = "1px solid black"
 div1.style.fontSize = "30px"
-div1.style.overflow = ""
+div1.style.overflow = "scroll"
+div1.style.textAlign = "center"
+// ============================================
+div2.style.width = "300px"
+div2.style.height = "400px"
+div2.style.border = "1px solid black"
+div2.style.fontSize = "30px"
+div2.style.overflow = "scroll"
+div2.style.textAlign = "center"
+// =================================================
+div3.style.width = "300px"
+div3.style.height = "400px"
+div3.style.border = "1px solid black"
+div3.style.fontSize = "30px"
+div3.style.overflow = "scroll"
+div3.style.textAlign = "center"
+// =============================================
+div4.style.width = "300px"
+div4.style.height = "400px"
+div4.style.border = "1px solid black"
+div4.style.fontSize = "30px"
+div4.style.overflow = "scroll"
+div4.style.textAlign = "center"
+
