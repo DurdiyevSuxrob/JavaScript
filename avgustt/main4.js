1b7e39adb94bd4a8cb9e34f03a853160638d6360
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-13 23:14:01 +0500

    ADD

--- avgustt/main4.js
+++ avgustt/main4.js
@@ -1,15 +1,3 @@
-const div1 = document.getElementById('div1')
-div1.addEventListener("click", () => {
-    console.log("Sichqonchani chap tarafni bir marta bosdingiz !");
-    div1.style.backgroundColor = "red"
-    div1.style.width = "600px"
-    div1.style.height = "200px"
-    div1.style.transition = "1s"
-    div1.innerHTML =`
-    <button style = "width: 100px; height: 60px; border: none; background-color: green; color: white;">Send</button>
-
-    `
-})
 const div2 = document.getElementById('div2')
 div2.addEventListener("dblclick", () => {
      console.log("Sichqonchani chap tarafni ikki marta bosdingiz !");
