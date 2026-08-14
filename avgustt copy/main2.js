6314613db65f79e5a92544501127fc604cc03d59
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-14 08:50:01 +0500

    add

---
+++ avgustt copy/main2.js
@@ -0,0 +1,76 @@
+const div = document.getElementById('div')
+const div1 = document.getElementById('div1')
+const div2 = document.getElementById('div2')
+const div3 = document.getElementById('div3')
+const div4 = document.getElementById('div4')
+const bosh = document.getElementById('bosh')
+
+
+// ==========================================================
+div.style.width = "1900px"
+div.style.height = "600px"
+div.style.margin = "auto"
+div.style.marginTop = "100px"
+div.style.display = "flex"
+div.style.justifyContent = "space-around"
+div.style.alignContent = "center"
+div.style.alignItems = "center"
+div.style.borderRadius = "30px"
+div.style.boxShadow = "0px 0px 10px 4px black"
+
+// =============================================
+div1.style.width = "300px"
+div1.style.height = "400px"
+div1.style.fontSize = "21px"
+div1.style.fontWeight = "900"
+div1.style.overflow = "scroll"
+div1.style.textAlign = "center"
+div1.style.borderTopLeftRadius = "25px"
+div1.style.boxShadow = "0px 0px 10px 4px black"
+
+// ============================================
+div2.style.width = "300px"
+div2.style.height = "400px"
+div2.style.border = "1px solid black"
+div2.style.fontSize = "21px"
+div2.style.overflow = "scroll"
+div2.style.textAlign = "center"
+div2.style.fontWeight = "900"
+div2.style.boxShadow = "0px 0px 10px 3px black"
+div2.style.borderTopLeftRadius = "25px"
+
+
+// =================================================
+div3.style.width = "300px"
+div3.style.height = "400px"
+div3.style.border = "1px solid black"
+div3.style.fontSize = "21px"
+div3.style.overflow = "scroll"
+div3.style.textAlign = "center"
+div3.style.fontWeight = "900"
+div3.style.boxShadow = "0px 0px 10px 3px black"
+div3.style.borderTopLeftRadius = "25px"
+
+// =============================================
+div4.style.width = "300px"
+div4.style.height = "400px"
+div4.style.border = "1px solid black"
+div4.style.fontSize = "21px"
+div4.style.overflow = "scroll"
+div4.style.textAlign = "center"
+div4.style.fontWeight = "900"
+div4.style.boxShadow = "0px 0px 10px 3px black"
+div4.style.borderTopLeftRadius = "25px"
+
+// =============================================
+bosh.style.textAlign = "center"
+bosh.style.marginTop = "50px"
+bosh.style.fontSize = "50px"
+bosh.style.fontWeight = "1000"
+bosh.style.color = "blue"
+bosh.style.boxShadow = "0px 0px 5px 20px blue"
+bosh.style.backgroundColor = "red"
+bosh.style.borderRadius = "25px"
+
+
+
