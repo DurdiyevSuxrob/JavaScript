a765c0239d58bd8e0148eff86020bebfd7a6b23c
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-15 22:37:27 +0500

    add

--- avgustt/main4.js
+++ avgustt/main4.js
@@ -7,7 +7,7 @@
 // const arr = document.getElementById('add')
 // button1.addEventListener("click", () => {
 //     console.log("ishladi");
-    
+
 //     button1.innerHTML = `
 //     <button style = "
 //     width: 300px;
@@ -18,20 +18,49 @@
 //     transition: 1s;
 //     border: none;
 //     ">✅ Tolov Qilindi!</button>
-    
+
 //     `
 //     arr.play()
 // })
 // ======================================
-const ret1 = document.getElementById(ret1)
-const ret2 = document.getElementById(ret2)
-const ret3 = document.getElementById(ret3)
-const ret4 = document.getElementById(ret4)
-const ret5 = document.getElementById(ret5)
-const ret5 = document.getElementById(ret5)
+const ret1 = document.getElementById("ret1");
+const ret2 = document.getElementById("ret2");
+const ret3 = document.getElementById("ret3");
+const ret4 = document.getElementById("ret4");
+const ret5 = document.getElementById("ret5");
+const ret6 = document.getElementById("ret6");
 
-const add1 = document.getElementById('add1')
+const add1 = document.getElementById("add1");
+const add2 = document.getElementById("add2");
+const add3 = document.getElementById("add3");
+const add4 = document.getElementById("add4");
+const add5 = document.getElementById("add5");
+const add6 = document.getElementById("add6");
 
 
 
 
+
+ret1.addEventListener("click", () => {
+  add1.play();
+});
+const add2 = document.getElementById("add2");
+ret2.addEventListener("click", () => {
+  add2.play();
+});
+const add3 = document.getElementById("add3");
+ret3.addEventListener("click", () => {
+  add3.play();
+});
+const add4 = document.getElementById("add4");
+ret4.addEventListener("click", () => {
+  add4.play();
+});
+const add5 = document.getElementById("add5");
+ret5.addEventListener("click", () => {
+  add5.play();
+});
+const add6 = document.getElementById("add6");
+ret6.addEventListener("click", () => {
+  add6.play();
+});
