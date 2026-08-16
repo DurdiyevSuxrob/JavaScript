cc8f9c57532e991ea6208fa2bcfba108b4de803b
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-16 16:29:36 +0500

    asdd

--- avgustt/main4.js
+++ avgustt/main4.js
@@ -30,14 +30,48 @@ const ret4 = document.getElementById("ret4");
 const ret5 = document.getElementById("ret5");
 const ret6 = document.getElementById("ret6");
 const add1 = document.getElementById("add1");
+const add2 = document.getElementById("add2");
+const add3 = document.getElementById("add3");
+const add4 = document.getElementById("add4");
+const add5 = document.getElementById("add5");
+const add6 = document.getElementById("add6");
 
 
 
 
 
-add1.addEventListener("click", () => {
-  add1.play();
+
+
+
+ret1.addEventListener("click", () => {
+  add1.play()
   console.log("ishladi");
   
 });
+ret3.addEventListener("click", () => {
+  add2.play()
+  console.log("ishladi");
+  
+});
+ret2.addEventListener("click", () => {
+  add3.play()
+  console.log("ishladi");
+  
+});
+ret4.addEventListener("click", () => {
+  add4.play()
+  console.log("ishladi");
+  
+});
+ret5.addEventListener("click", () => {
+  add5.play()
+  console.log("ishladi");
+  
+});
+ret6.addEventListener("click", () => {
+  add6.play()
+  console.log("ishladi");
+  
+});
+
 
