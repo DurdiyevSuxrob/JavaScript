620bd74df54b8c0d90e57088a80f2278712b3267
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-17 22:17:09 +0500

    add

---
+++ avgustt/main6.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("btn")
+ 
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("drak")
+
+    if (document.body.classList.contains("drak")) {
+        btn.style.fontSize = "46px"
+        btn.textContent = "🌑"
+    } else {
+        btn.style.fontSize = "46px"
+        btn.textContent = "☀️"
+    }
+})
