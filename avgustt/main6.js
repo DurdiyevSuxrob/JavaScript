7ea28300446be1f861e418d7808c44a786593de9
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-08-21 12:15:23 +0500

    add

--- avgustt/main6.js
+++ avgustt/main6.js
@@ -1,12 +1,12 @@
-const nav = document.getElementById("nav")
+
 const btn = document.getElementById('btn')
 btn.addEventListener("click", () => {
     document.body.classList.toggle("light")
 
     if (document.body.classList.contains("light")) {
-        btn.textContent = "☀️ Dark Mode"
+        btn.textContent = "🌑 Dark Mode"
     } else {
-        btn.textContent =  "🌙 Dark Mode"
+        btn.textContent =  "☀️ Dark Mode"
     }
     
 })
