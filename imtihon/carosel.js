4d3c3a26078078598120b443db2803beff2d86c3
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-09-18 17:39:53 +0500

    add

--- imtihon/carosel.js
+++ imtihon/carosel.js
@@ -1,20 +1,34 @@
 const trak = document.getElementById("track");
 const prevBtn = document.getElementById("prevBtn");
 const nexBtn = document.getElementById("nexBtn");
+const card = document.querySelectorAll(".card");
 
+const cardLength = card.length
 let hisoblagich = 0;
 
 const cardwidth = 210;
 
 nexBtn.addEventListener("click", () => {
     if (hisoblagich > 0) {
+        hisoblagich++;
+        updateCarousel();
+    }
+});
+
+prevBtn.addEventListener("click", () => {
+    if (hisoblagich > 0) {
         hisoblagich--;
         updateCarousel();
     }
 });
 
+function updateCarousel() {
+    const moveAmount = -hisoblagich * cardwidth;
+    trak.style.transform = `translateX(${moveAmount}px)`;
+}
 
-fun
+
+
 
 
 
