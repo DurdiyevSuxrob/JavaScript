0f6dd9e6bf11fe055888bf79ec3123c5e51bb74c
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-09-18 17:31:36 +0500

    add

--- imtihon/carosel.js
+++ imtihon/carosel.js
@@ -2,7 +2,19 @@ const trak = document.getElementById("track");
 const prevBtn = document.getElementById("prevBtn");
 const nexBtn = document.getElementById("nexBtn");
 
+let hisoblagich = 0;
 
+const cardwidth = 210;
+
+nexBtn.addEventListener("click", () => {
+    if (hisoblagich > 0) {
+        hisoblagich--;
+        updateCarousel();
+    }
+});
+
+
+fun
 
 
 
