c80fc5abc55dc6a4458573006c72550eb0030959
DurdiyevSuxrob <sdurdiyev24@gmail.com>
2026-09-19 20:43:49 +0500

    add

--- imtihon/carosel.js
+++ imtihon/carosel.js
@@ -3,75 +3,28 @@ const prevBtn = document.getElementById("prevBtn");
 const nexBtn = document.getElementById("nexBtn");
 const card = document.querySelectorAll(".card");
 
-const cardLength = card.length
+const cardLength = card.length;
 let hisoblagich = 0;
 
 const cardwidth = 210;
 
 nexBtn.addEventListener("click", () => {
-    if (hisoblagich > 0) {
-        hisoblagich++;
-        updateCarousel();
-    }
+  if (hisoblagich < cardLength) {
+    hisoblagich++;
+    updateCarousel();
+  }
 });
 
 prevBtn.addEventListener("click", () => {
-    if (hisoblagich > 0) {
-        hisoblagich--;
-        updateCarousel();
-    }
+  if (hisoblagich > 0) {
+    hisoblagich--;
+    updateCarousel();
+  }
 });
 
 function updateCarousel() {
-    const moveAmount = -hisoblagich * cardwidth;
-    trak.style.transform = `translateX(${moveAmount}px)`;
+  const moveAmount = -hisoblagich * cardwidth;
+  trak.style.transform = `translateX(${moveAmount}px)`;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-c
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+c;
